454ca1070b79412e687d6dec1eca68eaf8cd2254
Anand Palanisamy <96760059+AnandP-Tringapps@users.noreply.github.com>
2022-05-06 14:04:11 +0530

    Update Footer.js

--- task-5-redux/src/Footer.js
+++ task-5-redux/src/Footer.js
@@ -3,7 +3,7 @@ import {useDispatch, useSelector} from 'react-redux';
 export default function Footer() {
 const initialvalue = useSelector(state => state);
 
-const dispatch = useDispatch();
+
   return (
     <div id='footer' class="footer">
     <h2>{initialvalue[1]}</h2>
